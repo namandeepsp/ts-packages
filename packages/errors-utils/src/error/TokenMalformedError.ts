048628f78b975b6d7d11c173be6139428699255c
Naman deep singh <namandeepsp@gmail.com>
2025-12-22 16:41:45 +0000

    Updated some error classes to simplify error handling.

--- packages/errors-utils/src/error/TokenMalformedError.ts
+++ packages/errors-utils/src/error/TokenMalformedError.ts
@@ -1,8 +1,9 @@
 import { HTTP_STATUS } from '@naman_deep_singh/response-utils'
 import { ERROR_CODES } from 'src/constants'
 import { UnauthorizedError } from './UnauthorizedError'
+import { HTTPError } from './HTTPError'
 
-export class TokenMalformedError extends UnauthorizedError {
+export class TokenMalformedError extends HTTPError {
 	constructor(details?: unknown, cause?: Error) {
 		super(
 			ERROR_CODES.TOKEN_INVALID,

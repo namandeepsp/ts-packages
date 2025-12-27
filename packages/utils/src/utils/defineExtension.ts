ed457d0b9344e0c9a217367d82cd8bc0b890b4b8
Naman deep singh <namandeepsp@gmail.com>
2025-12-27 09:52:34 +0000

    Fixed biome errors in utils package.

--- packages/utils/src/utils/defineExtension.ts
+++ packages/utils/src/utils/defineExtension.ts
@@ -1,14 +1,14 @@
 export function defineExtension<T extends object>(
-    prototype: T,
-    name: string,
-    fn: Function,
+	prototype: T,
+	name: string,
+	fn: Function,
 ) {
-    if (!Object.prototype.hasOwnProperty.call(prototype, name)) {
-        Object.defineProperty(prototype, name, {
-            value: fn,
-            writable: false,
-            configurable: false,
-            enumerable: false,
-        })
-    }
+	if (!Object.prototype.hasOwnProperty.call(prototype, name)) {
+		Object.defineProperty(prototype, name, {
+			value: fn,
+			writable: false,
+			configurable: false,
+			enumerable: false,
+		})
+	}
 }
